10953e87d1e77cb0bda98ac97d9d75bfacb23d16
biruktadele <biruktadele55@gmail.com>
2025-07-31 04:48:57 -0300

    feat: implement product repository using remote and local data sources

--- mobile/Biruk_Tadele/ecommerce/lib/features/product_catalog/data/repositories/product_repository_impl.dart
+++ mobile/Biruk_Tadele/ecommerce/lib/features/product_catalog/data/repositories/product_repository_impl.dart
@@ -1,4 +1,80 @@
 import 'package:dartz/dartz.dart';
 
+import '../../../../core/error/failure.dart';
 import '../../domain/entities/product.dart';
 import '../../domain/repositories/product_repository.dart';
+import '../datasources/product_remote_data_source.dart';
+import '../datasources/product_local_data_source.dart';
+
+import '../../../../core/error/failure.dart';
+
+class ProductRepositoryImpl implements ProductRepository {
+  final ProductRemoteDataSource remoteDataSource;
+  final ProductLocalDataSource localDataSource;
+
+  ProductRepositoryImpl({
+    required this.remoteDataSource,
+    required this.localDataSource,
+  });
+
+ 
+  @override
+  Future<Either<Failure, List<Product>>> getAllProducts() async {
+    try {
+      final remoteProducts = await remoteDataSource.getAllProducts();
+      await localDataSource.cacheProducts(remoteProducts);
+      return Right(remoteProducts);
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Product>> getProductById(int id) async {
+    try {
+      final cachedProduct = await localDataSource.getCachedProduct(id);
+      return Right(cachedProduct);
+    } catch (e) {
+      try {
+        final remoteProduct = await remoteDataSource.getProductById(id);
+        await localDataSource.cacheProduct(remoteProduct);
+        return Right(remoteProduct);
+      } catch (e) {
+        return Left(ServerFailure(e.toString()));
+      }
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> addProduct(Product product) async {
+    try {
+      await remoteDataSource.addProduct(product);
+      await localDataSource.cacheProduct(product);
+      return Right(null);
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> updateProduct(Product product) async {
+    try {
+      await remoteDataSource.updateProduct(product);
+      await localDataSource.cacheProduct(product);
+      return Right(null);
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> deleteProduct(int id) async {
+    try {
+      await remoteDataSource.deleteProduct(id);
+      await localDataSource.removeCachedProduct(id);
+      return Right(null);
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+}
